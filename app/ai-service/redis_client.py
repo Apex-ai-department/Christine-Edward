2c369ef0a95a1e88f5df67cc7c00fd2ba6cd262d
Edward Wang <edjackwang@gmail.com>
2025-06-26 13:51:13 +0900

    fixes so it finaly works

--- app/ai-service/redis_client.py
+++ app/ai-service/redis_client.py
@@ -2,8 +2,8 @@ import redis
 
 # Connect to cloud Redis (replace placeholders)
 redis_client = redis.Redis(
-    host='viable-ram-48359.upstash.io/',  # From your token
-    port=6370,                 # From your token
+    host='viable-ram-48359.upstash.io',  # From your token
+    port=6379,                 # From your token
     password='AbznAAIjcDFlMGI2NDg3NDc1OWQ0YTZhOGI2MzQ0NzlmZTE3YTEwM3AxMA', # From your token
     ssl=True                    # Usually required for cloud Redis
 )
